8e8ec68cd96187cf503d430ba4b092167ab02741
pawelkaczor <newion@o2.pl>
2021-07-29 22:21:17 +0200

    ScalaTest Template with kanela agent

--- src/main/scala/pl/newicom/ide/intellij/IntellijSbtTasksPlugin.scala
+++ src/main/scala/pl/newicom/ide/intellij/IntellijSbtTasksPlugin.scala
@@ -4,21 +4,34 @@ import sbt._
 
 object IntellijSbtTasksPlugin extends AutoPlugin {
 
-  private val taskFileNames: Seq[String] = Seq(
-    "/publishLocal.run.xml",
-    "/compile.run.xml",
-    "/recompile.run.xml",
-    "/tests.run.xml",
-    "/integrationTests.run.xml",
-  )
-
-  private val tasks: Seq[(String, String)] =
-    taskFileNames.map(f => (f, IO.readStream(IntellijSbtTasksPlugin.getClass.getResourceAsStream(f))))
-
   override def trigger = allRequirements
+
+  private val commonTasks =
+    Seq(
+      "publishLocal.run.xml",
+      "compile.run.xml",
+      "recompile.run.xml",
+      "tests.run.xml",
+      "integrationTests.run.xml"
+    )
+      .map(readResource)
+
+  private val scalaTestWithKanelaAgentTemplate =
+    readResource("kamon/Template ScalaTest.run.xml")
+
+  object autoImport {
+    val kamonInstrumentationEnabled = settingKey[Boolean]("Enable Kamon instrumentation")
+  }
+
+  import autoImport._
+
+  override lazy val globalSettings: Seq[Setting[_]] = Seq(
+    kamonInstrumentationEnabled := false,
+  )
 
   override def buildSettings: Seq[Def.Setting[_]] = {
     SettingKey[Unit]("intellijGenerateSbtTasks") := {
+      val tasks = if (kamonInstrumentationEnabled.value) commonTasks :+ scalaTestWithKanelaAgentTemplate else commonTasks
       tasks.foreach {
         case (fileName, task) =>
           val f = file(".run" + fileName)
@@ -27,6 +40,11 @@ object IntellijSbtTasksPlugin extends AutoPlugin {
           }
       }
     }
+  }
+
+  private def readResource: String => (String, String) = { path =>
+    val fileName = path.split('/').toSeq.fold(path) { case (_, r) => r }
+    (s"/$fileName", IO.readStream(IntellijSbtTasksPlugin.getClass.getResourceAsStream(s"/$path")))
   }
 
 }
